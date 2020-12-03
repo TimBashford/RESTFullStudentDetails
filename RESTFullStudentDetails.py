91abe769567191914b92356bdc1097e53ad5ea13
Tim Bashford <Tim.Bashford@uwtsd.ac.uk>
2020-12-03 17:07:03 +0000

    Replaced single link with call to course endpoint and links to degreetype and coursename

--- RESTFullStudentDetails.py
+++ RESTFullStudentDetails.py
@@ -1,1 +1,23 @@
+import requests, json
 
+def PrintCourse(courseURL):
+    r = requests.get(courseURL)
+
+    if r.status_code != 200:
+        print("course: course not found.")
+    else:
+        courseJson = json.loads(r.content)
+        degreeTypeURL = courseJson['_links']['degreetype']['href']
+        courseNameURL = courseJson['_links']['coursename']['href']
+        print("course: {0}\t{1}".format(degreeTypeURL, courseNameURL))
+
+r = requests.get('http://5.67.108.54:8080/student/')
+
+studentListJson = json.loads(r.content)
+
+for student in studentListJson['_embedded']['student']:
+    print("name: {0}".format(student['name']))
+    print("address: {0}".format(student['address']))
+    print("dob: {0}".format(student['dob']))
+    PrintCourse(student['_links']['course']['href'])
+    print("\n")
